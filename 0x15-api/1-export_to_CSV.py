f3307ac4fca0ea9b963a4d8f8df20da325d3552e
DaisyAram <daisyarama03@gmail.com>
2024-04-22 05:20:42 -0700

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,15 +10,12 @@ import sys
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    if todos:
-        with open('{}.csv'.format(user_id), 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-            [writer.writerow(
+    with open('{}.csv'.format(user_id), 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
                 [user_id, username, t.get("completed"), t.get("title")]
                 ) for t in todos]
-    else:
-        print(f"No tasks found for user ID: {user_id}")
