24af294ea3fa49bc5be5b94f69ec48a1c2301fe3
DaisyAram <daisyarama03@gmail.com>
2024-05-07 09:47:17 -0700

    pycodestyle

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-queries Reddit API and prints the titles of the first 10 hot 
+queries Reddit API and prints the titles of the first 10 hot
 posts listed for a given subreddit
 """
 
@@ -15,14 +15,14 @@ def top_ten(subreddit):
     headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     params = {"limit": 10}
 
-    response = requests.get(url, headers=headers, params= params, allow_redirects=False)
-    
+    response = requests.get(
+            url, headers=headers, params=params, allow_redirects=False
+            )
+
     if response.status_code == 200:
         data = response.json().get("data")
 
         for i in data.get("children", []):
             print(i.get("data").get("title"))
-
-        
     else:
-            print(None)
+        print(None)
