8684483cc662f50d1c7a9f19cf89e20c35df6b8c
DaisyAram <daisyarama03@gmail.com>
2024-05-07 06:30:18 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""queries Reddit API and returns the number of subscribers"""
-
-
+""" script that queries the Reddit API and returns the number of subscribers """
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """returns number of subscribers"""
-    response = requests.get(
-            'https://www.reddit.com/r/{}/about.json'.format(subreddit), {
-                'User-Agent': 'Custom'}
-            )
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
-        return 0
+	""" returns number of subscribers"""
+	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+	headers = {"user-Agent": "google/1.0"}
+	response = requests.get(url, headers=headers, allow_redirects=False)
+	if response.status_code == 200:
+		data = response.json()
+		subscribers = data['data']['subscribers']
+		return subscribers
+	else:
+		return 0
