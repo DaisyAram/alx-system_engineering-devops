353ec9deb3e6234a556e1af55556335bdbdd925e
DaisyAram <daisyarama03@gmail.com>
2024-05-07 02:52:40 -0700

    updates

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""queries Reddit API and returns the number of subscribers"""
+
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """returns number of subscribers"""
+    response = requests.get(
+            'https://www.reddit.com/r/{}/about.json'.format(subreddit), {
+                'User-Agent': 'Custom'}
+            )
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
